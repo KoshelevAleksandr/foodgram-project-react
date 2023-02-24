2928cc10c9a02f4bc7cb22fd9ed33bdaacffbffc
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2023-02-25 00:28:32 +0300

    s

--- backend/recipes/management/commands/import_bd.py
+++ backend/recipes/management/commands/import_bd.py
@@ -7,7 +7,7 @@ from recipes.models import Ingredient
 
 file_ingredient = os.path.join(
     os.path.dirname(settings.BASE_DIR),
-    'backend_static/data/ingredients.csv')
+    'app/backend_static/data/ingredients.csv')
 file_tags = os.path.join(
     os.path.dirname(settings.BASE_DIR),
     'backend_static/data/tags.csv')
