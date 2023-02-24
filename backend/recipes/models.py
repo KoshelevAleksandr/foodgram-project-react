313959960a13df1e7ca2a1b0abc87da97b9765e7
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2023-02-25 00:46:42 +0300

    asd

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -38,7 +38,7 @@ class Ingredient(models.Model):
     name = models.CharField(
         verbose_name='Название ингредиента',
         max_length=200,
-        unique=True)
+    )
     measurement_unit = models.CharField(
         verbose_name='Единица измерения',
         max_length=10)
@@ -47,6 +47,9 @@ class Ingredient(models.Model):
         ordering = ['name']
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            UniqueConstraint(fields=['name', 'measurement_unit'],
+                             name='unique_ingredient')]
 
     def __str__(self):
         return f'{self.name}, {self.measurement_unit}'
